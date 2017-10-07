0f65ca3e34d77050a186db6f3340c8266eb46886
Austin <steeleshellsword@gmail.com>
2017-10-07 04:02:08 +0000

    Added total products, average price, total cost.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,14 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @count = @products.count
+    @avg_price = 0
+    @total_stock = 0
+
+    @products.each { |product|
+      @avg_price += product.price
+      @total_stock += product.stock_quantity
+    }
   end
 
   # GET /products/1
